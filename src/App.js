5ed73f80a45fe6f30f584d2f6484b34b364e7e7f
Aaron Bjork <aaronbjork@hotmail.com>
2019-01-09 07:00:14 -0800

    Starter project initialized and ready. AB#282

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. We're all hooked up and ready to go!
           </p>
           <a
             className="App-link"
