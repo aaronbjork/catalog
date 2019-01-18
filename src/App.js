76241814e474e798d78d8083fe32aa2ecf05a7ff
Aaron Bjork <aaronbjork@hotmail.com>
2019-01-18 09:35:02 -0800

    This change fixes AB#299

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            We're all hooked up and ready to go!!!
+            This is the best home page.
           </p>
           <a
             className="App-link"
